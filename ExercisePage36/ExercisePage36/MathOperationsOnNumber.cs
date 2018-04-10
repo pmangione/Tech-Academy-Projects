db0a0e6a5dc6fd7226b0e45ed779bd9f8cb625ef
Peter <Peter@example.com>
2018-04-10 16:53:19 -0400

    Added Exercise Page 36 to Tech Adacemy Projects
    Added Exercise Page 36 to Tech Adacemy Projects

---
+++ ExercisePage36/ExercisePage36/MathOperationsOnNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisePage36
+{
+    class MathOperationsOnNumber
+    {
+        public int subtractTwentyFromNumber(int userNumber)
+        {
+            return userNumber - 20;
+        }
+
+        public int multiplyNumberByTwo(int userNumber)
+        {
+            return userNumber * 2;
+        }
+
+        public int addOneToNumber(int userNumber)
+        {
+            return userNumber + 1;
+        }
+    }
+}
