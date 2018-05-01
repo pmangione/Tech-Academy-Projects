f061fbf31613abcfcd3a0ef4a49ccece7662e07e
Peter <Peter@example.com>
2018-05-01 12:01:06 -0400

    Adding TwentyOneProject to Master
    Adding TwentyOneFile to Master

---
+++ TwentyOne/Casino/Dealer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Casino
+{
+    public class Dealer
+    {
+        public string Name { get; set; }
+        public Deck Deck { set; get; }
+        public int Balance { set; get; } 
+
+        public void Deal(List<Card> Hand)
+        {
+            Hand.Add(Deck.Cards.First());
+
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+            Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\pmang\OneDrive\Documents\CodeCamp\TwentyOneFolderText\log.txt", true))
+            {
+                file.WriteLine(DateTime.Now);
+                file.WriteLine(card);
+            }
+
+
+            Console.WriteLine(Deck.Cards.First().ToString() + "\n");
+            Deck.Cards.RemoveAt(0);
+        }
+
+    }
+}
---
+++ TwentyOne/TwentyOne/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Casino;
+using Casino.TwentyOne;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string casinoName = "Grand Hotel and Casino";
+
+            Console.WriteLine("Welcome to the {0}.  Let's start by telling me your name.", casinoName);
+            string playerName = Console.ReadLine();
+            
+
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("And how much money did you bring today?");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals.");
+            }
+
+            Console.WriteLine("Hello {0}, would you like to join a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "y" || answer == "ya" || answer == "yes" || answer == "yeah")
+            {
+                Player player = new Player(playerName,bank);
+
+                player.Id = Guid.NewGuid();
+                
+                using (StreamWriter file = new StreamWriter(@"C:\Users\pmang\OneDrive\Documents\CodeCamp\TwentyOneFolderText\log.txt", true))
+                {
+                    file.WriteLine(player.Id);
+                }
+
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing.");
+            }
+            Console.WriteLine("Feel free to look around the casino.  Bye for now.");
+            Console.Read();
+        }
+
+       
+    }
+}
